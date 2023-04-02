a5cbf41d7a194d69fc10e26d77468f2d3ff5d4f0
Ahmed M. Znaty <ahmdmznaty@gmail.com>
2023-04-02 17:23:19 +0200

    VideoPlyWebsite

---
+++ VideoPlyWebsite/ReactJS/src/index.js
@@ -0,0 +1,14 @@
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Home from "./scripts/Pages/Home";
+
+let root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+    <Home />
+)
+
+
+
+
